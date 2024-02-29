af09ef14d10346de8ced2d154f0d522b4a7ba08f
Shasten <shastenm76@gmail.com>
2024-02-29 16:04:47 -0700

    attached language servers to mason

--- lua/plugins/mason/linter.lua
+++ lua/plugins/mason/linter.lua
@@ -1,39 +1,36 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.completion.luasnip,
-				null_ls.builtins.formatting.stylua,
-				-- null_ls.builtins.diagnostics.codespell,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.curlylint,
-				null_ls.builtins.diagnostics.checkmake,
-				null_ls.builtins.diagnostics.djlint,
-				null_ls.builtins.diagnostics.jsonlint,
-				null_ls.builtins.diagnostics.php,
-				null_ls.builtins.diagnostics.hadolint,
-				null_ls.builtins.formatting.gofmt,
-				null_ls.builtins.diagnostics.revive,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.pyflyby,
-				-- null_ls.builtins.diagnostics.ruff,
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.diagnostics.shellcheck,
-				--        null_ls.builtins.diagnostics.spectral,
-				null_ls.builtins.diagnostics.tidy,
-				null_ls.builtins.formatting.nginx_beautifier,
-				null_ls.builtins.formatting.ocamlformat,
-				null_ls.builtins.formatting.rustfmt,
-				null_ls.builtins.formatting.rustywind,
-				null_ls.builtins.formatting.stylish_haskell,
-				null_ls.builtins.formatting.terraform_fmt,
-				null_ls.builtins.formatting.xmllint,
-			},
-		})
-
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.completion.luasnip,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.curlylint,
+        null_ls.builtins.diagnostics.checkmake,
+        null_ls.builtins.diagnostics.djlint,
+        null_ls.builtins.diagnostics.jsonlint,
+        null_ls.builtins.diagnostics.php,
+        null_ls.builtins.diagnostics.hadolint,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.diagnostics.ruff,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.diagnostics.tidy,
+        null_ls.builtins.formatting.nginx_beautifier,
+        null_ls.builtins.formatting.ocamlformat,
+        null_ls.builtins.formatting.rustfmt,
+        null_ls.builtins.formatting.rustywind,
+        null_ls.builtins.formatting.stylish_haskell,
+        null_ls.builtins.formatting.terraform_fmt,
+        null_ls.builtins.formatting.xmllint,
+      },
+    })
+    local key = vim.keymap.set
+    local buf = vim.lsp.buf.format
+    key("n", "<leader>gf", buf, {})
+  end,
 }
--- lua/plugins/mason/lsp.lua
+++ lua/plugins/mason/lsp.lua
@@ -20,13 +20,82 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
+      lspconfig.bashls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.biome.setup({
+        capabilities = capabilities
+      })
+      lspconfig.cmake.setup({
+        capabilities = capabilities
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.dockerls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.hls.setup({
         capabilities = capabilities
       })
       lspconfig.html.setup({
         capabilities = capabilities
       })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ltex.setup({
+        capabilities = capabilities
+      })
       lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.nginx_language_server.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ocamlls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.phpactor.setup({
+        capabilities = capabilities
+      })
+      lspconfig.prosemd_lsp.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ruff_lsp.setup({
+        capabilities = capabilities
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities
+      })
+      lspconfig.sqlls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities
+      })
+      lspconfig.taplo.setup({
+        capabilities = capabilities
+      })
+      lspconfig.terraformls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.typos_lsp.setup({
+        capabilities = capabilities
+      })
+      lspconfig.volar.setup({
+        capabilities = capabilities
+      })
+      lspconfig.zls.setup({
         capabilities = capabilities
       })
 
